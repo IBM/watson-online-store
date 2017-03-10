71edd4473a34af5d2f3efc6a03d38ef31b2f611e
Mark Sturdevant <mark.sturdevant@ibm.com>
2017-03-10 11:07:37 -0800

    Add query to Discovery when it is configured.
    If all the necessary env vars are set, Discovery
    is added.
    
    For now, it is always called when avilable and the
    top selected result snippets (etc) are appended to
    the response.
    
    The full results are logged for dev/test.

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from cloudant.client import Cloudant
 from dotenv import load_dotenv
 from slackclient import SlackClient
 from watson_developer_cloud import ConversationV1
+from watson_developer_cloud import DiscoveryV1
 
 from watsononlinestore.watson_online_store import WatsonOnlineStore
 from database.cloudant_online_store import CloudantOnlineStore
@@ -33,9 +34,29 @@ if __name__ == "__main__":
         os.environ.get("CLOUDANT_DB_NAME")
     )
 
+  #
+  # Init Watson Discovery only if all the env vars are set.
+  #
+  discovery_username = os.environ.get('DISCOVERY_USERNAME')
+  discovery_password = os.environ.get('DISCOVERY_PASSWORD')
+  discovery_environment_id = os.environ.get('DISCOVERY_ENVIRONMENT_ID')
+  discovery_collection_id = os.environ.get('DISCOVERY_COLLECTION_ID')
+
+  discovery_client = None
+  if all((discovery_username,
+          discovery_password,
+          discovery_environment_id,
+          discovery_collection_id)):
+
+      discovery_client = DiscoveryV1(
+          version='2016-11-07',
+          username=discovery_username,
+          password=discovery_password)
+
   watsononlinestore = WatsonOnlineStore(bot_id,
                                   slack_client,
                                   conversation_client,
+                                  discovery_client,
                                   cloudant_online_store)
 
   watsononlinestore.run()
